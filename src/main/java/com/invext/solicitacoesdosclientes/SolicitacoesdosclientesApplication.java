7a586e94bdc984da125daa8a54ab63b4792a5223
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2024-09-20 11:24:41 -0300

    [INVEXT][MASTER][MOD] criacao de projeto

---
+++ src/main/java/com/invext/solicitacoesdosclientes/SolicitacoesdosclientesApplication.java
@@ -0,0 +1,13 @@
+package com.invext.solicitacoesdosclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolicitacoesdosclientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolicitacoesdosclientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/invext/solicitacoesdosclientes/SolicitacoesdosclientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invext.solicitacoesdosclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolicitacoesdosclientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
